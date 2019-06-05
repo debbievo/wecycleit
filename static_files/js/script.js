767c2fe532c69dddbda9db57b55f15b52d78b047
Debbie Vo <debbienvo@yahoo.com>
2019-06-04 23:42:48 -0700

    Half finished autocomplete

--- static_files/js/script.js
+++ static_files/js/script.js
@@ -1,100 +1,118 @@
 $(document).ready(() => {
-    let parseURL = new URLSearchParams(document.location.search);
+  initializePage();
+});
+let parseURL = new URLSearchParams(document.location.search);
 
-    let user_material;
-    let user_zip;
-    let user_dist = 10;
-    let user_num_results = 10;
-    let user_dropoff = true;
-    let user_pickup = false;
+let user_material;
+let user_zip;
+let user_dist = 10;
+let user_num_results = 10;
+let user_dropoff = true;
+let user_pickup = false;
 
-    if(parseURL.has('material')) {
-        user_material = parseURL.get('material');
-    }
-    if(parseURL.has('zip')) {
-        user_zip = parseURL.get('zip');
-    }
-    if(parseURL.has('distance')) {
-        user_dist = parseURL.get('distance');
-    }
-    if(parseURL.has('numresults')) {
-        user_num_results = parseURL.get('numresults');
-    }
-    if(parseURL.has('dropoff')) {
-        user_dropoff = parseURL.get('dropoff');
-    }
-    if(parseURL.has('pickup')) {
-        user_pickup = parseURL.get('pickup');
-    }
-    if(parseURL.has('material')) {
-        user_material = parseURL.get('material');
-    }
-    if(parseURL.has('zip')) {
-        user_zip = parseURL.get('zip');
-    }
+if(parseURL.has('material')) {
+    user_material = parseURL.get('material');
+}
+if(parseURL.has('zip')) {
+    user_zip = parseURL.get('zip');
+}
+if(parseURL.has('distance')) {
+    user_dist = parseURL.get('distance');
+}
+if(parseURL.has('numresults')) {
+    user_num_results = parseURL.get('numresults');
+}
+if(parseURL.has('dropoff')) {
+    user_dropoff = parseURL.get('dropoff');
+}
+if(parseURL.has('pickup')) {
+    user_pickup = parseURL.get('pickup');
+}
+if(parseURL.has('material')) {
+    user_material = parseURL.get('material');
+}
+if(parseURL.has('zip')) {
+    user_zip = parseURL.get('zip');
+}
+var availableTags = [
+      "aerosol cans",
+      "aluminum",
+      "bottles",
+      "bottle",
+      "cans",
+      "metal",
+      "plastic",
+      "plastics",
+      "battery",
+      "batteries",
+    ];
 
-    $('#material_box').val(user_material);
-    $('#zipBox').val(user_zip);
+function initializePage(){
+  $( "#material_box" ).autocomplete({
+    source: availableTags
+  });
 
-    $('#material_box').keyup(function(event) {
-        if (event.keyCode === 13 ) {
-            $('#searchButton').click();
-        }
-    });
+  $('#material_box').val(user_material);
+  $('#zipBox').val(user_zip);
 
-    $('#zipBox').keyup(function(event) {
-        if(event.keyCode === 13) {
-            $('#searchButton').click();
-        }
-    });
+  $('#material_box').keyup(function(event) {
+      if (event.keyCode === 13 ) {
+          $('#searchButton').click();
+      }
+  });
 
-    $("#searchButton").click(function() {
+  $('#zipBox').keyup(function(event) {
+      if(event.keyCode === 13) {
+          $('#searchButton').click();
+      }
+  });
+
+  $("#searchButton").click(function() {
+      let user_material = $('#material_box').val();
+      let zip_code = $('#zipBox').val();
+
+      if(!user_material) {
+          alert("Please type in what you want to recycle.")
+      }
+      else if(!zip_code){
+          alert("Please type in a zip code.")
+      }
+      else {
+          window.document.location = './searchResultsListView.html' + '?material=' + user_material
+          + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
+          + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
+      }
+  });
+  $("#backButton").click(function() {
         let user_material = $('#material_box').val();
         let zip_code = $('#zipBox').val();
+        let prevWindow = document.referrer;
 
-        if(!user_material) {
-            alert("Please type in what you want to recycle.")
-        }
-        else if(!zip_code){
-            alert("Please type in a zip code.")
-        }
-        else {
+        let parseURL = new URLSearchParams(prevWindow);
+
+        if(parseURL.has('index')){
+                window.document.location = './index.html' + '?material=' + user_material
+                        + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
+                        + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
+        } else if (parseURL.has('searchResultsListView')) {
             window.document.location = './searchResultsListView.html' + '?material=' + user_material
-            + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
-            + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
+                    + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
+                    + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
+        } else if (parseURL.has('searchResultsMapView')) {
+            window.document.location = './searchResultsMapView.html' + '?material=' + user_material
+                    + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
+                    + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
+        // } else if (parseURL.has('centerInfo')) {
+        //     window.document.location = './centerInfo.html' + '?material=' + user_material
+        //             + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
+        //             + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup + '&centerName=' + center_name
+        //             + '&phone='+ center_phone + '&materials=' + center_materials + '&hours=' + center_hours + '&address=' + arr[0];
+
+        }else {
+            window.history.back();
         }
     });
-    $("#backButton").click(function() {
-          let user_material = $('#material_box').val();
-          let zip_code = $('#zipBox').val();
-          let prevWindow = document.referrer;
-
-          let parseURL = new URLSearchParams(prevWindow);
-
-          if(parseURL.has('index')){
-                  window.document.location = './index.html' + '?material=' + user_material
-                          + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
-                          + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
-          } else if (parseURL.has('searchResultsListView')) {
-              window.document.location = './searchResultsListView.html' + '?material=' + user_material
-                      + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
-                      + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
-          } else if (parseURL.has('searchResultsMapView')) {
-              window.document.location = './searchResultsMapView.html' + '?material=' + user_material
-                      + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
-                      + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup;
-          // } else if (parseURL.has('centerInfo')) {
-          //     window.document.location = './centerInfo.html' + '?material=' + user_material
-          //             + '&zip=' + zip_code + "&distance=" + user_dist + '&numresults=' + user_num_results
-          //             + '&dropoff=' + user_dropoff + '&pickup=' + user_pickup + '&centerName=' + center_name
-          //             + '&phone='+ center_phone + '&materials=' + center_materials + '&hours=' + center_hours + '&address=' + arr[0];
-
-          }else {
-              window.history.back();
-          }
-      });
-
-});
+}
 
 function goBack() {
   window.history.back();
